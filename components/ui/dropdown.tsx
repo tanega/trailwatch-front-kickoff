4e76021e69fd06104044bf25061c2eda4e71a8ab
Gaëtan DUHAMEL <gaetan_duhamel@natural-solutions.eu>
2024-04-10 13:11:57 +0200

    fix: add type to FormElement

--- components/ui/dropdown.tsx
+++ components/ui/dropdown.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { FormEvent } from "react"
+
 type Props = {
   options: string[]
   className?: string
@@ -10,11 +12,13 @@ export default function Dropdown({ className, options, onSelect }: Props) {
   return (
     <form
       className={className}
-      onChange={(event) => onSelect(event.target.value)}
+      onChange={(event: FormEvent<HTMLFormElement>) =>
+        onSelect((event.target as HTMLSelectElement).value)
+      }
     >
       <select
         id="countries"
-        className="w-full bg-color-3 p-1 text-xxs text-white"
+        className="p-1 w-full text-white bg-color-3 text-xxs"
       >
         {options.map((option) => (
           <option key={option} value={option}>
