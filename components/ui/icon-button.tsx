1998098aea131ad6e7644da101d61368d0515158
Gaëtan DUHAMEL <gaetan_duhamel@natural-solutions.eu>
2024-04-18 15:40:00 +0200

    fix(ui): clean unused imports

--- components/ui/icon-button.tsx
+++ components/ui/icon-button.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import Image from "next/image"
 
 type Props = {
   description: string
@@ -15,7 +14,7 @@ const IconButton: React.FC<Props> = ({ onClick, children, description }) => {
       className="me-2 inline-flex items-center rounded-full bg-white p-2.5 text-center text-sm font-medium text-black shadow-md hover:bg-slate-200 focus:outline-none focus:ring-4 focus:ring-slate-300 dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800"
     >
       {children}
-      <span className="sr-only">Icon description</span>
+      <span className="sr-only">{description}</span>
     </button>
   )
 }
